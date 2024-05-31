5d677261594b0c501af2f29d85e7a42b21fcc776
Francisco Peres <nos7manjibr@gmail.com>
2024-05-30 21:18:14 -0300

    changing list

--- src/modules/components/List.tsx
+++ src/modules/components/List.tsx
@@ -1,4 +1,4 @@
 export function List() {
-  const arrayList = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+  const arrayList = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
   return arrayList.map((item) => <li key={item}>{item}</li>);
 }
